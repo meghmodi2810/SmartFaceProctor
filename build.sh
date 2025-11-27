1d3ba0444b64e38994e2c62d41e63b120cd3d258
Megh <23bmiit146@gmail.com>
2025-11-27 19:37:43 +0530

    Build successful, error in deployment, added build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# filepath: build.sh
+
+set -o errexit
+
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Navigate to Django project directory
+cd proctor
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run migrations
+python manage.py migrate
