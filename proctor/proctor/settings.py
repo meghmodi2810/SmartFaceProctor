a6e60eafd4c54fdefb87be124339a2ae943496ea
Megh <23bmiit146@gmail.com>
2025-11-27 20:07:39 +0530

    Postgres for deployment, mysql for local

--- proctor/proctor/settings.py
+++ proctor/proctor/settings.py
@@ -107,23 +107,35 @@ WSGI_APPLICATION = 'proctor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'proctor_db',
-        'USER': 'root',
-        'PASSWORD': '6640',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / "db.sqlite3",
-#     }
-# }
+
+import os
+ENV = os.environ.get("ENV", "local")
+
+if ENV != 'render':
+    # Local development - MySQL
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'proctor_db',
+            'USER': 'root',
+            'PASSWORD': '6640',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
+    }
+else:
+    # Production on Render - PostgreSQL
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT', '5432'),
+        }
+    }
 
 
 # Password validation
